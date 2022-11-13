6d6437d03641a30fe96307851cbe32ecfb473f47
danylovanin <danylo.vanin@modex.one>
2022-11-13 20:07:28 +0200

    Added advanced queries

--- advanced_queries.py
+++ advanced_queries.py
@@ -76,3 +76,50 @@ def get_top10_2product_combinations_over_period(start_date: datetime, end_date: 
         print(row)
         result.append(row)
     return result
+
+
+# Вивести топ 10 купівель товарів по три за період С (наприклад молоко, масло, хліб - 1000 разів)
+def get_top10_3product_combinations_over_period(start_date: datetime, end_date: datetime, db: Session):
+    statement = text(f"""
+    select s.product_id as product_1, 
+       s2.product_id as product_2, 
+       s3.product_id as product_3,
+       count(*) as number_of_sales, 
+       string_agg(s.receipt_id::varchar, ',') as receipts 
+    from sales s join sales s2 on s.product_id < s2.product_id join sales s3 on s2.product_id < s3.product_id 
+    where s.receipt_id = s2.receipt_id and s2.receipt_id = s3.receipt_id 
+        and s.receipt_id in (select id from receipts r where r."date">{start_date.isoformat()} and r."date"<{end_date.isoformat()})
+    group by s.product_id ,s2.product_id , s3.product_id 
+    order by number_of_sales desc
+    fetch first 10 rows only;""")
+
+    result_set = db.execute(statement)
+    result = []
+    for row in result_set:
+        print(row)
+        result.append(row)
+    return result
+
+
+# Вивести топ 10 купівель товарів по чотири за період С
+def get_top10_4product_combinations_over_period(start_date: datetime, end_date: datetime, db: Session):
+    statement = text(f"""
+    select s.product_id as product_1, 
+       s2.product_id as product_2, 
+       s3.product_id as product_3,
+       s4.product_id as product_4,
+       count(*) as number_of_sales, 
+       string_agg(s.receipt_id::varchar, ',') as receipts 
+    from sales s join sales s2 on s.product_id < s2.product_id join sales s3 on s2.product_id < s3.product_id join sales s4 on s3.product_id < s4.product_id 
+    where s.receipt_id = s2.receipt_id and s2.receipt_id = s3.receipt_id and s3.receipt_id = s4.receipt_id 
+        and s.receipt_id in (select id from receipts r where r."date">{start_date.isoformat()} and r."date"<{end_date.isoformat()})
+    group by s.product_id ,s2.product_id , s3.product_id , s4.product_id 
+    order by number_of_sales desc
+    fetch first 10 rows only;""")
+
+    result_set = db.execute(statement)
+    result = []
+    for row in result_set:
+        print(row)
+        result.append(row)
+    return result
